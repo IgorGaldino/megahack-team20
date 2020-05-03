a15ba7123ee550a226240a2a88d9c707cd2470d2
Matheus Gurgel <matheuskillua77@gmail.com>
2020-05-03 15:26:44 -0300

    navbar

--- src/layout/Navbar.vue
+++ src/layout/Navbar.vue
@@ -1,35 +1,22 @@
 <template>
-	<div>
-        <b-button>asd</b-button>
-		<b-navbar fixed="top" toggleable="lg" type="dark" variant="primary">
-			<router-link to="/">
-
-				<!-- <img src="@/assets/logo.svg" alt="Minha Figura" />
-				<img class="fontImage" src="@/assets/font.svg" alt="Minha Figura" /> -->
-			</router-link>
-			<button
-				type="button"
-				aria-label="Toggle navigation"
-				aria-controls="nav-collapse"
-				aria-expanded="false"
-				class="navbar-toggler"
-			>
-				<span class="navbar-toggler-icon"></span>
-			</button>
-
-			<b-collapse id="nav-collapse" is-nav>
-				<b-navbar-nav class="ml-auto">
-					<b-nav-item right>
-						<div class="d-flex align-items-start">
-							<a style="color: #fff; padding-right: 10px; padding-top: 5px;"
-								>Sair</a
-							>
-						</div>
-					</b-nav-item>
-				</b-navbar-nav>
-			</b-collapse>
-		</b-navbar>
-	</div>
+<div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Facilite</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <template v-slot:button-content>
+            <img class="rounded-circle" src="http://placehold.it/40x40" alt="">
+          </template>
+          <b-dropdown-item href="#">Configurar Perfil</b-dropdown-item>
+		  <b-dropdown-item href="#">Minha Loja</b-dropdown-item>
+          <b-dropdown-item href="#">Sair</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
 </template>
 
 <script>
