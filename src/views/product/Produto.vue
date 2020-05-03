14a93f581bd74af36e82fe282c42ad5e5289e455
Luan Douglas <douglasmaia.mn.sl@gmail.com>
2020-05-03 17:15:20 -0300

    produto

--- src/views/product/Produto.vue
+++ src/views/product/Produto.vue
@@ -3,12 +3,45 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-3">
-					<h1 class="my-4">Shop Name</h1>
-					<div class="list-group">
-						<a href="#" class="list-group-item">Category 1</a>
-						<a href="#" class="list-group-item">Category 2</a>
-						<a href="#" class="list-group-item">Category 3</a>
-					</div>
+					<img src="http://placehold.it/230x230" class="mt-4 rounded-circle" />
+					<h3 class="my-4">Pizzaria 2 Irmãos</h3>
+					<ul class="list-group">
+						<li
+							type="button"
+							class="list-group-item d-flex justify-content-between align-items-center"
+						>
+							Pizza
+							<span class="badge badge-primary badge-pill">14</span>
+						</li>
+						<li
+							type="button"
+							class="list-group-item d-flex justify-content-between align-items-center"
+						>
+							Hamburguer
+							<span class="badge badge-primary badge-pill">2</span>
+						</li>
+						<li
+							type="button"
+							class="list-group-item d-flex justify-content-between align-items-center"
+						>
+							Salgados
+							<span class="badge badge-primary badge-pill">2</span>
+						</li>
+						<li
+							type="button"
+							class="list-group-item d-flex justify-content-between align-items-center"
+						>
+							Bebidas quentes
+							<span class="badge badge-primary badge-pill">8</span>
+						</li>
+						<li
+							type="button"
+							class="list-group-item d-flex justify-content-between align-items-center"
+						>
+							Bebidas frias
+							<span class="badge badge-primary badge-pill">4</span>
+						</li>
+					</ul>
 				</div>
 				<!-- /.col-lg-3 -->
 
@@ -56,24 +89,14 @@
 								/>
 							</div>
 						</div>
-						<a
-							class="carousel-control-prev"
-							href="#carouselExampleIndicators"
-							role="button"
-							data-slide="prev"
-						>
+						<a class="carousel-control-prev" role="button" data-slide="prev">
 							<span
 								class="carousel-control-prev-icon"
 								aria-hidden="true"
 							></span>
 							<span class="sr-only">Previous</span>
 						</a>
-						<a
-							class="carousel-control-next"
-							href="#carouselExampleIndicators"
-							role="button"
-							data-slide="next"
-						>
+						<a class="carousel-control-next" role="button" data-slide="next">
 							<span
 								class="carousel-control-next-icon"
 								aria-hidden="true"
@@ -85,21 +108,17 @@
 					<div class="row">
 						<div class="col-lg-4 col-md-6 mb-4">
 							<div class="card h-100">
-								<a href="#"
+								<a
 									><img
 										class="card-img-top"
 										src="http://placehold.it/700x400"
 										alt=""
 								/></a>
 								<div class="card-body">
-									<h4 class="card-title">
-										<a href="#">Item One</a>
-									</h4>
-									<h5>$24.99</h5>
-									<p class="card-text">
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-										Amet numquam aspernatur!
-									</p>
+									<h5 class="card-title">
+										<a>Pizza de Calabresa</a>
+									</h5>
+									<h5>R$27.99</h5>
 								</div>
 								<div class="card-footer">
 									<small class="text-muted"
@@ -111,21 +130,17 @@
 
 						<div class="col-lg-4 col-md-6 mb-4">
 							<div class="card h-100">
-								<a href="#"
+								<a
 									><img
 										class="card-img-top"
 										src="http://placehold.it/700x400"
 										alt=""
 								/></a>
 								<div class="card-body">
-									<h4 class="card-title">
-										<a href="#">Item Two</a>
-									</h4>
-									<h5>$24.99</h5>
-									<p class="card-text">
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-										Amet numquam aspernatur! Lorem ipsum dolor sit amet.
-									</p>
+									<h5 class="card-title">
+										<a>Pizza Mussarela</a>
+									</h5>
+									<h5>R$22.98</h5>
 								</div>
 								<div class="card-footer">
 									<small class="text-muted"
@@ -137,21 +152,17 @@
 
 						<div class="col-lg-4 col-md-6 mb-4">
 							<div class="card h-100">
-								<a href="#"
+								<a
 									><img
 										class="card-img-top"
 										src="http://placehold.it/700x400"
 										alt=""
 								/></a>
 								<div class="card-body">
-									<h4 class="card-title">
-										<a href="#">Item Three</a>
-									</h4>
-									<h5>$24.99</h5>
-									<p class="card-text">
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-										Amet numquam aspernatur!
-									</p>
+									<h5 class="card-title">
+										<a>Pizza Marguerita</a>
+									</h5>
+									<h5>R$24.99</h5>
 								</div>
 								<div class="card-footer">
 									<small class="text-muted"
@@ -163,21 +174,17 @@
 
 						<div class="col-lg-4 col-md-6 mb-4">
 							<div class="card h-100">
-								<a href="#"
+								<a
 									><img
 										class="card-img-top"
 										src="http://placehold.it/700x400"
 										alt=""
 								/></a>
 								<div class="card-body">
-									<h4 class="card-title">
-										<a href="#">Item Four</a>
-									</h4>
-									<h5>$24.99</h5>
-									<p class="card-text">
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-										Amet numquam aspernatur!
-									</p>
+									<h5 class="card-title">
+										<a>Pizza de Carne de sol</a>
+									</h5>
+									<h5>R$27.99</h5>
 								</div>
 								<div class="card-footer">
 									<small class="text-muted"
@@ -189,21 +196,17 @@
 
 						<div class="col-lg-4 col-md-6 mb-4">
 							<div class="card h-100">
-								<a href="#"
+								<a
 									><img
 										class="card-img-top"
 										src="http://placehold.it/700x400"
 										alt=""
 								/></a>
 								<div class="card-body">
-									<h4 class="card-title">
-										<a href="#">Item Five</a>
-									</h4>
-									<h5>$24.99</h5>
-									<p class="card-text">
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-										Amet numquam aspernatur! Lorem ipsum dolor sit amet.
-									</p>
+									<h5 class="card-title">
+										<a>Pizza Mista</a>
+									</h5>
+									<h5>R$19.99</h5>
 								</div>
 								<div class="card-footer">
 									<small class="text-muted"
@@ -215,21 +218,17 @@
 
 						<div class="col-lg-4 col-md-6 mb-4">
 							<div class="card h-100">
-								<a href="#"
+								<a
 									><img
 										class="card-img-top"
 										src="http://placehold.it/700x400"
 										alt=""
 								/></a>
 								<div class="card-body">
-									<h4 class="card-title">
-										<a href="#">Item Six</a>
-									</h4>
+									<h5 class="card-title">
+										<a>Pizza Franco Catupiry</a>
+									</h5>
 									<h5>$24.99</h5>
-									<p class="card-text">
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-										Amet numquam aspernatur!
-									</p>
 								</div>
 								<div class="card-footer">
 									<small class="text-muted"
